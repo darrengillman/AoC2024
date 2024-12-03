bf82adf465f34bcdff4bf8fec398b03e52413c2a
Darren Gillman <darren@toppuffin.co.uk>
2024-12-03 14:34:55 +0000

    Day3 Pt2 complete

--- Tests/Day03Tests.swift
+++ Tests/Day03Tests.swift
@@ -16,18 +16,19 @@
   struct SolutionsTests {
     @Suite("Tests on sample inputs")
     struct SolutionsTests {
-      let day = Day03(data: testInput)
+      let dayp1 = Day03(data: testInput1)
+      let dayp2 = Day03(data: testInput2)
 
       @Test("Part1")
       func testPart1() async throws {
-          let result = try await day.part1()
+          let result = try await dayp1.part1()
           #expect(result == 161)
       }
 
       @Test("Part2 example")
       func testPart2() async throws {
         await withKnownIssue {
-          let result = try await day.part2()
+          let result = try await dayp2.part2()
           #expect(result == 10)
         }
       }
@@ -35,7 +36,12 @@
   }
 }
 
-private let testInput =
+private let testInput1 =
   """
   xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
   """
+
+private let testInput2 =
+"""
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+"""
