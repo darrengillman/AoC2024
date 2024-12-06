37ebe67a461d4cd2abc37b7d1dd102a2fafdd914
Darren Gillman <darren@toppuffin.co.uk>
2024-12-06 19:43:03 +0000

    day5 parts 1 & 2 done

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -1,20 +1,62 @@
 import Foundation
+import AoCTools
 
 struct Day05: AdventDay, Sendable {
-  // Save your data in a corresponding text file in the `Data` directory.
-  let data: String
-  let day = 0
-  let puzzleName: String = "--- Day 0: Placeholder! ---"
-
-  init(data: String) {
-    self.data = data
-  }
-
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() async throws -> Int {
-    0
-  }
+   typealias Page = Int
+   let day = 0
+   let puzzleName: String = "--- Day 5 ---"
+   var rules: [Page: Set<Page>] = [:]
+   var books: [[Page]] = []
+   
+   init(data: String) {
+      data
+         .components(separatedBy: .newlines)
+         .forEach { line in
+            if line.contains("|") {
+               let components = line.components(separatedBy: "|")
+               rules[Int(components[0])!, default: Set<Int>()].insert(Int(components[1])!)
+            } else if line.contains(",") {
+               books.append(line.components(separatedBy: ",").map{Int($0)!})
+            }
+         }
+   }
+   
+      // Replace this with your solution for the first part of the day's challenge.
+   func part1() async throws -> Int {
+      books
+         .filter{isOrdered($0)}
+         .map{ $0[$0.count/2] }
+         .reduce(0, +)
+   }
+   
+   func part2() async throws -> Int {
+      books
+         .filter{!isOrdered($0)}
+         .map{ book in
+            book.sorted { first, second in
+               guard let onlyAfterSecond = rules[second] else {return true}
+               return !onlyAfterSecond.contains(first)
+            }
+         }
+         .map{ $0[$0.count/2] }
+         .reduce(0, +)
+   }
+   
+   func isOrdered(_ pages: [Int]) -> Bool {
+      var prior = Set<Page>()
+      
+      for current in pages {
+         if let aheadOnly = rules[current], prior.intersection(aheadOnly).isEmpty == false {
+            return false
+         }
+         prior.insert(current)
+      }
+      return true
+   }
+                           
+                           
+                           
 }
 
-// Add any extra code and types in here to separate it from the required behaviour
+   // Add any extra code and types in here to separate it from the required behaviour
 extension Day05 {}
--- Tests/Day05Tests.swift
+++ Tests/Day05Tests.swift
@@ -17,21 +17,26 @@
     @Suite("Tests on sample inputs")
     struct SolutionsTests {
       let day = Day05(data: testInput)
+       
+       @Test("Parsing")
+       func parsingTest() {
+          let pages = day.books.count
+          #expect( pages == 6)
+          let numRules = day.rules.keys.count
+          #expect(numRules == 6)
+       }
+       
 
-      @Test("Part1 example")
+      @Test("Part1")
       func testPart1() async throws {
-        await withKnownIssue {
           let result = try await day.part1()
-          #expect(result == 10)
-        }
+          #expect(result == 143)
       }
 
       @Test("Part2 example")
       func testPart2() async throws {
-        await withKnownIssue {
           let result = try await day.part2()
-          #expect(result == 10)
-        }
+          #expect(result == 123)
       }
     }
   }
@@ -39,4 +44,32 @@
 
 private let testInput =
   """
+  47|53
+  97|13
+  97|61
+  97|47
+  75|29
+  61|13
+  75|53
+  29|13
+  97|29
+  53|29
+  61|53
+  97|53
+  61|29
+  47|13
+  75|47
+  97|75
+  47|61
+  75|61
+  47|29
+  75|13
+  53|13
+  
+  75,47,61,53,29
+  97,61,53,29,13
+  75,29,13
+  75,97,47,61,53
+  61,13,29
+  97,13,75,29,47
   """
